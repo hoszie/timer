87218da5bb3bf57a833c90f5700f9b71b91116dc
Nick <nhoszko@gmail.com>
2019-10-23 01:53:43 +0000

    dope interactive timer

---
+++ timer2.js
@@ -0,0 +1,19 @@
+const stdin = process.stdin;
+stdin.setRawMode(true);
+stdin.setEncoding('utf8');
+
+stdin.on('data', (key) => {
+  if (key === '\u0003') {
+    console.log("Thanks for using me, ciao!");
+    process.exit();
+  }
+  if (key === 'b') {
+    process.stdout.write("\x07"); 
+  }
+    if (key === "1" || key === "2" || key === "3" || key === "4" || key === "5" || key === "6" || key === "7" || key === "8" || key === "9") {
+      process.stdout.write(`setting timer for ${key} seconds...\n`);
+      setTimeout(() => {
+            process.stdout.write("\x07"); 
+          }, key * 1000)
+    }
+})
